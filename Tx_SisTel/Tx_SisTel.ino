c05246b6453d3f7adf587d828113b154e5fc8714
Alaf Santos <alaf.ns4@gmail.com>
2020-11-13 16:23:19 -0300

    transmissor quase pronto, falta soh o cod754 - funcao ta retornando 0 por enquanto

--- Tx_SisTel/Tx_SisTel.ino
+++ Tx_SisTel/Tx_SisTel.ino
@@ -1,80 +1,123 @@
 /*
- * Alunos: Alaf Santos / Matheus Boy
+ * Alunos: Alaf Santos / Lucas Chemelli / Matheus Boy
  * Disciplina: Sistemas de Telecomunicacoes
  * Codigo para Transmissor VLC
+ * line code that maybe I'll need (it is just about an error that I got): sudo chmod a+rw /dev/ttyACM0
  */
- 
-#define LED 10        //saida analogica na qual o led esta conectado
-#define ONE   255     //maximo para analogWrite (8 bits) - 5V
-#define ZERO  200     //minimo para nao termos uma variacao visivel e condizer com a sensibilidade do receptor - funfa pra teste
+
+#define SENSOR A0   //usar um potenciometro pra simular um sensor qualquer
+#define LED 10     //Pino onde saira o sinal pros leds
+#define N2  255
+#define N1  227 
+#define N0  200     
 #define WAIT 5      //200Hz - sincronizado com o receptor (pseudo clk)
 
-String header = "00" + String('#', BIN); //(#)10 = (00100011)2, somei os zeros do inicio para termos 8 bits
-String footer = "00" + String('$', BIN); //($)10 = (00100100)2, somei o zero do inicio para termos 8 bits
-String input = "00000101"; //5 em binario de 8 bits
+String StringI3E754(float sensor);
+void sendData(String pktIEEE754);
+void sendPar(String pktIEEE754);
+void sendPKT754(String pktIEEE754);
+void sendStart();
+void sendStop();
+
 
 void setup() 
 {
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
+  pinMode(SENSOR, INPUT);
 }
 
 void loop() 
 {
- if(Serial.available()) //o envio comeca ao entrarmos com algo no serial
- {
-  for(int j = 0; j < header.length(); j++)
+ analogWrite(LED, N1);
+ delay(500);
+ float sensor = analogRead(SENSOR);
+ Serial.print("Leitura Sensor: ");
+ Serial.println(sensor);
+
+ String pktIEEE754 = StringI3E754(sensor); //neste ponto ja temos a string de bits
+ sendData(pktIEEE754);
+
+}
+
+String StringI3E754(float sensor)
+{
+  //implementar
+  return "0";
+}
+
+void sendData(String pktIEEE754)
+{
+ sendStart();
+ sendPKT754(pktIEEE754);
+ sendPar(pktIEEE754);
+ sendStop();
+
+}
+
+void sendStart()
+{
+  for(int j = 0; j < 4; j++)
   {
-      Serial.println(header);
-      if(header[j] == '1')
-      { 
-        analogWrite(LED, ONE);
-        Serial.println("UM");
-        delay(WAIT);
-      }
-      else 
-      {
-        analogWrite(LED, ZERO);
-        Serial.println("ZERO");
-        delay(WAIT);
-      }  
-      
+     analogWrite(LED, N0);
+     Serial.println("N0");
+     delay(WAIT);   
   }
-     
-  for(int i = 0; i < input.length(); i++)
+}
+
+void sendStop()
+{
+  for(int j = 0; j < 4; j++)
   {
-    Serial.println(input);
-    if(input[i] == '1')
-    { 
-      analogWrite(LED, ONE);
+     analogWrite(LED, N1);
+     Serial.println("N1");
+     delay(WAIT);   
+  }
+}
+
+void sendPar(String pktIEEE754)
+{
+  int counter = 0;
+  for(int i = 0; i < pktIEEE754.length(); i++)
+    if(pktIEEE754[i] == '1')
+      counter++;
+  if(!(counter%2))
+  {
       Serial.println("UM");
-      delay(WAIT);
+      analogWrite(LED, N2);
+      delay(WAIT/2);
+      analogWrite(LED, N0);
+      delay(WAIT/2);
+  }
+  else
+  {
+      Serial.println("ZERO");
+      analogWrite(LED, N0);
+      delay(WAIT/2);
+      analogWrite(LED, N2);
+      delay(WAIT/2); 
+  }
+}
+
+void sendPKT754(String pktIEEE754)
+{
+  for(int i = 0; i < pktIEEE754.length(); i++)
+  {
+    if(pktIEEE754[i] == '1')
+    { 
+      Serial.println("UM");
+      analogWrite(LED, N2);
+      delay(WAIT/2);
+      analogWrite(LED, N0);
+      delay(WAIT/2);
     }
     else 
     {
-      analogWrite(LED, ZERO);
       Serial.println("ZERO");
-      delay(WAIT);
+      analogWrite(LED, N0);
+      delay(WAIT/2);
+      analogWrite(LED, N2);
+      delay(WAIT/2);
     }  
-  }
-
-  for(int j = 0; j < footer.length(); j++)
-  {
-      Serial.println(footer);
-      if(footer[j] == '1')
-      { 
-        analogWrite(LED, ONE);
-        Serial.println("UM");
-        delay(WAIT);
-      }
-      else 
-      {
-        analogWrite(LED, ZERO);
-        Serial.println("ZERO");
-        delay(WAIT);
-      }  
-      
-  }
-    Serial.println(" "); //pula uma linha
   }
 }
